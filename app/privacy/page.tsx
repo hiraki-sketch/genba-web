e70530699f115be8da4895ada6bb3fc5d0808bbe
hiraki <tomotaka111942@gmail.com>
2026-05-13 11:12:23 +0900

    修正 写真使用理由の追記

--- app/privacy/page.tsx
+++ app/privacy/page.tsx
@@ -26,10 +26,6 @@ export default function PrivacyPage() {
 
       <h2 className="text-xl font-bold mt-6 mb-2">
         利用目的
-        <li>画像投稿</li>
-        <li>現場の異常報告や設備状況を共有する為写真を添付する機能で
-          写真を使用します。
-        </li>
       </h2>
 
       <ul className="list-disc pl-6">
@@ -38,6 +34,23 @@ export default function PrivacyPage() {
         <li>異常報告・チャット機能の提供</li>
         <li>サービス改善</li>
       </ul>
+     <h2 className="text-xl font-bold mt-6 mb-2">
+      画像利用について
+      </h2>
+
+      <p>
+        ユーザーが投稿した画像は、業務連絡や異常報告の目的で利用されます。
+        これらの画像は、ユーザーの同意なしに第三者に提供されることはありません。
+      </p>
+      <h2 className="text-xl font-bold mt-6 mb-2">
+        データの保存について
+      </h2>
+      <p>本アプリでは、ユーザーが投稿した引き継ぎ情報、異常報告、
+      チャット内容、画像などのデータを、サービス提供および機能改善のために保存する場合があります。</p>
+      <p>保存されたデータは、ユーザーの同意なしに第三者に提供されることはありません。
+        認証及び
+        アクセス制御を行ったうえで管理されます。
+      </p>
 
       <h2 className="text-xl font-bold mt-6 mb-2">
         第三者提供
